e13051b40508a1f3f9eaa5a2d049f25eb52c0a00
Scott Howard <showard@hvccc.com.au>
2023-12-15 15:43:58 +1100

    day 1 finished  Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 if __name__ == "__main__":
-    #vars
-    user_prompt ="Enter a todo:"
-    #code block
-    print(user_prompt) 
-    user_text = input()
-    print(user_text)
+    user_prompt ="Enter a todo:
+    #userinput
+    todo1 = input(user_prompt)
+    todo2 = input(user_prompt)
+    todo3 = input(user_prompt)
+   #store as a list
+    todos = [todo1,todo2,todo3]
+    print(todos)
 
-
+    #print(type(user_prompt)) #test type of a variable
