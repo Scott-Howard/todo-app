a584326161d362e4a089564ae038f5d088c2afb4
Scott Howard <scottmelvillehoward@gmail.com>
2023-12-18 23:36:38 +1100

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 if __name__ == "__main__":
     user_prompt ="Enter a todo:"
-    #userinput
-    todo1 = input(user_prompt)
-    todo2 = input(user_prompt)
-    todo3 = input(user_prompt)
-   #store as a list
-    todos = [todo1,todo2,todo3]
-    print(todos)
 
-    #print(type(user_prompt)) #test type of a variable
+    todos = [] # initialise an emppty list
+    while True :
+        todo = input(user_prompt)
+        print(todo.capitalize())
+        todos.append(todo)
+        print(todos)
