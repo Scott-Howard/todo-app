07e1394c9c5392672fcc32991ade71e395c5b4df
Scott Howard <scottmelvillehoward@gmail.com>
2023-12-19 23:34:10 +1100

    Changes to be committed: 	modified:   main.py added for loop to print todos added error handling for whitespace input either side

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ if __name__ == "__main__":
     todos = [] # initialise an emppty list
     while True :
         user_action = input(user_prompt)
-
+        user_action = user_action.strip() #removes whitespace
         match user_action:
             case 'add':
                 todo = input("Enter a todo: ")
                 todos.append(todo)
             case 'show':
-                print(todos)   
+                #print(todos)
+                for item in todos: #nicely prints out the contents of todo
+                    print(item)   
             case 'exit':
                 break    
 print('Bye!')
