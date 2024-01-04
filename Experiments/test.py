e0cc6826ce57a01373d5afb8297acfc922485ac3
Scott Howard <showard@hvccc.com.au>
2024-01-04 15:51:49 +1100

    Changes to be committed: 	modified:   Experiments/e3.py 	modified:   Experiments/test.py

--- Experiments/test.py
+++ Experiments/test.py
@@ -1,7 +1,28 @@
-countries = []
+# countries = []
  
-while True:
-    country = input("Enter the country: ")
-    countries.append(country)
-    print(countries)
+# while True:
+#     country = input("Enter the country: ")
+#     countries.append(country)
+#     print(countries)
 
+# 
+
+# ranking = ['John', 'Sen', 'Lisa']
+
+# rank_out = int(input("Enter rank number: "))
+# print(ranking[rank_out])
+
+# seconds = [1.23, 1.45, 1.02]
+
+# current = 1.11
+
+
+# seconds.append(current)
+
+# elements = ['a', 'b', 'c']
+# print(elements[1])
+
+elements = ['a', 'b', 'c']
+new = 'x'
+elements[1] = new
+print(elements)
