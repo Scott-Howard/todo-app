231e2d38119a92dbd254b3d934f6ca1f6dfa5d42
Scott Howard <showard@hvccc.com.au>
2024-01-12 12:17:26 +1100

    add if/elif blocks to control flow
    Changes to be committed:
    	modified:   main.py
    	modified:   todos.txt

--- main.py
+++ main.py
@@ -1,60 +1,65 @@
 if __name__ == "__main__":
     user_prompt ="type add or show, edit, complete or exit: "
 
-    while True :
+    while True:
         user_action = input(user_prompt)
         user_action = user_action.strip() # removes whitespace
-        match user_action:
-            case 'add':
-                todo = input("Enter a todo: ") + "\n"
 
-                with open('todos.txt','r') as file:
-                    todos = file.readlines()
 
-                todos.append(todo)
+        if 'add' in user_action:
+            todo = user_action[4:] #list slice from just after "add "
 
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todos)
-                            
-            case 'show' : 
-                with open('todos.txt','r') as file:
-                    todos = file.readlines()
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
 
-                new_todos = [item.strip('\n') for item in todos] # list compehension
+            todos.append(todo + '\n')
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+                        
+        elif 'show' in user_action: 
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+
+            new_todos = [item.strip('\n') for item in todos] # list compehension
+            
+            for index,item in enumerate(new_todos): 
+                item = item.title()
+                row = f"{index + 1}. {item}"
+                print(row) 
+        elif 'exit' in user_action:
+            break
+        
+        elif 'complete' in user_action:
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+
+            number = int(user_action[9:])
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+            
+            message = f"Todo {todo_to_remove} was removed"
+            print(message)
                 
-                for index,item in enumerate(new_todos): 
-                    item = item.title()
-                    row = f"{index + 1}. {item}"
-                    print(row) 
-            case 'exit':
-                break
-            case 'complete':
-                with open('todos.txt','r') as file:
-                    todos = file.readlines()
+        elif 'edit'in user_action:
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
 
-                number = int(input("Number of the todo to complete: "))
-                index = number - 1
-                todo_to_remove = todos[index].strip('\n')
-                todos.pop(index)
+            number = int(user_action[5:]) -1
 
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todos)
-                
-                message = f"Todo {todo_to_remove} was removed"
-                print(message)
-                    
-            case 'edit':
-                with open('todos.txt', 'r') as file:
-                    todos = file.readlines()
+            existing_todo = todos[number] 
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo +'\n'
 
-                number = int(input("Number of the todo to edit: "))-1
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        else:
+            print("Command is not valid")
 
-                existing_todo = todos[number] 
-                new_todo = input("Enter a new todo: ")
-                todos[number] = new_todo +'\n'
-
-                with open('todos.txt', 'w') as file:
-                    file.writelines(todos)
 
 
 print('Bye!')
