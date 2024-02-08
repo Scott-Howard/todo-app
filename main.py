b8b5d63449c44a2f6536d78ee0514997e17a4466
Scott Howard <showard@hvccc.com.au>
2024-02-08 11:17:19 +1100

    added docstrings to main.py Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 if __name__=="__main__":
 
     def get_todos(filepath='todos.txt'):
+        """ Read a text file and return the list
+        of todo items"""
         with open(filepath,'r') as file_local:
             todos_local = file_local.readlines()
         return todos_local  
 
     def write_todos(input, filepath='todos.txt'):
+        """ Write todo items to a text file"""
         with open(filepath,'w') as file_local:
             file_local.writelines(input)        
 
