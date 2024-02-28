9deaf268669e2c8ad1102d27d31136184f25d04f
Scott Howard <showard@hvccc.com.au>
2024-02-28 14:44:07 +1100

    added theme and bug fixes for index error
    Changes to be committed:
    	modified:   gui.py
    	modified:   todos.txt

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 from modules import functions as fn
 import PySimpleGUI as sg
+import time
 
+sg.theme("HotDogStand")
+
+clock =  sg.Text('', key= "clock")
 label = sg.Text("Type in a todo.")
 input = sg.InputText(tooltip="Enter a todo", key = "todo")
 
@@ -10,44 +14,52 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 list_box = sg.Listbox(values = fn.get_todos(), key = 'todos', enable_events = True,
-                      size = [45,10])
+                      size = [60,8])
 
 window = sg.Window('Todo App', 
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input,add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font = ("Helvetica", 16)) # layout needs to be a list of lists for layout widgets, each row is a list and need to be widget types.
 
 while True:
-    event,values = window.read()
-    print(1,event)
-    print(2,values)
-    print(3,values['todos'])
+    event,values = window.read(timeout=500)
+    window["clock"].update(value = time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(1,event)
+    # print(2,values)
+    # print(3,values['todos'])
     match event:
         case "Add":
             todos = fn.get_todos()
-            new_todo = values['todo'] + "\n"
+            new_todo = values['todo'] #+ "\n"
             todos.append(new_todo)
             fn.write_todos(todos)
             window['todos'].update(values = todos)
             window['todo'].update(value='')
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + "\n" #bug copuing m,ultiple times as a todo too many /n
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + "\n" #bug copuing m,ultiple times as a todo too many /n
 
-            todos = fn.get_todos()
-            index =todos.index(todo_to_edit)
-            todos[index] = new_todo
-            fn.write_todos(todos)
-            window['todos'].update(values = todos)
+                todos = fn.get_todos()
+                index =todos.index(todo_to_edit)
+                todos[index] = new_todo
+                fn.write_todos(todos)
+                window['todos'].update(values = todos)
+            except IndexError:
+                sg.popup("Select a todo to edit", font = ("Helvetica",20))
         case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos =fn.get_todos()
-            todos.remove(todo_to_complete)
-            fn.write_todos(todos)
-            window['todos'].update(values = todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos =fn.get_todos()
+                todos.remove(todo_to_complete)
+                fn.write_todos(todos)
+                window['todos'].update(values = todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Select a todo to complete", font = ("Helvetica",20))
         case 'todos':
             window['todo'].update(value = values['todos'][0])
         case "Exit":
